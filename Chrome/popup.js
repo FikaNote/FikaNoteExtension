3f0d03da9c93a30807a1cbc75e3d156502e4fa00
Kosuke Nagano <gm.charlie@gmail.com>
2015-06-10 20:02:00 +0900

    Modify to post URL

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -54,13 +54,15 @@ function getCurrentTabUrl(callback) {
  * @param {function(string)} errorCallback - Called when the image is not found.
  *   The callback gets a string that describes the failure reason.
  */
-function getImageUrl(searchTerm, callback, errorCallback) {
+function postCurrentUrl(currentUrl, callback, errorCallback) {
   // Google image search - 100 searches per day.
   // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
+  var postUrl = 'https://fikanote.herokuapp.com/agenda/';
   var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
+  x.open('POST', postUrl);
+  x.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  x.send(JSON.stringify({url:currentUrl}));
+
   // The Google image search API responds with JSON, so let Chrome parse it.
   x.responseType = 'json';
   x.onload = function() {
@@ -94,25 +96,16 @@ function renderStatus(statusText) {
 
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+    // Post URL
+    renderStatus('Post URL :' + url);
 
-    getImageUrl(url, function(imageUrl, width, height) {
+    postCurrentUrl(url, function(status, siteTitle, siteUrl) {
 
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
+      renderStatus('Status: ' + status+ '\n' +
+          'Title: ' + siteTitle);
 
     }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+      renderStatus('Error:' + errorMessage);
     });
   });
 });
